6ad9e9fc859d44990d65a515b921f1282df28b52
Karen Bu <ktianbu@gmail.com>
2025-02-21 19:40:01 +0000

    Finished exercise object-manipulation-1

--- object-manipulation-1/src/to-object.ts
+++ object-manipulation-1/src/to-object.ts
@@ -1,7 +1,7 @@
 /* exported toObject */
 
 function toObject(keyValuePair: (string | any)[]): object {
-  const passedObject: object = {};
-  passedObject[keyValuePair[0]] = keyValuePair[1];
-  return passedObject;
+  const newObject: object = {};
+  newObject[keyValuePair[0]] = keyValuePair[1];
+  return newObject;
 }
