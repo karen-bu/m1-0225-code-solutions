5eed4601450eebdbda11fc5a2bd93b7e2bd68a38
Karen Bu <ktianbu@gmail.com>
2025-03-19 05:01:51 +0000

    Finished exercise dom-manipulation

---
+++ dom-manipulation/main.ts
@@ -0,0 +1,31 @@
+let buttonClicks = 0;
+
+const $hotButton = document.querySelector('.hot-button');
+const $clickCount = document.querySelector('.click-count');
+
+if (!$hotButton) throw new Error('$hotButton does not exist');
+
+$hotButton.addEventListener('click', incrementClicks);
+
+function incrementClicks(): void {
+  if (!$hotButton || !$clickCount) {
+    throw new Error('The $hotButton or $clickCount query failed');
+  }
+
+  $clickCount.textContent = 'Clicks: ' + (buttonClicks += 1);
+  console.log('clickcount:', buttonClicks);
+
+  if (buttonClicks < 4) {
+    $hotButton.className = 'hot-button cold';
+  } else if (buttonClicks < 7) {
+    $hotButton.className = 'hot-button cool';
+  } else if (buttonClicks < 10) {
+    $hotButton.className = 'hot-button tepid';
+  } else if (buttonClicks < 13) {
+    $hotButton.className = 'hot-button warm';
+  } else if (buttonClicks < 16) {
+    $hotButton.className = 'hot-button hot';
+  } else if (buttonClicks >= 16) {
+    $hotButton.className = 'hot-button nuclear';
+  }
+}
