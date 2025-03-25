9c17f1bbc01ca63b6cff6d5e7f3af56c6040d769
Karen Bu <ktianbu@gmail.com>
2025-03-25 04:01:07 +0000

    Finished exercise typescript-light-bulb

---
+++ typescript-light-bulb/main.ts
@@ -0,0 +1,23 @@
+const $lightSwitch = document.querySelector('.dot.on');
+if (!$lightSwitch) throw new Error('$lightSwitch does not exist!');
+
+const $backgroundColor = document.querySelector('.container.on');
+if (!$backgroundColor) throw new Error('$backgroundColor does not exist!');
+
+$lightSwitch?.addEventListener('click', () => {
+  if ($lightSwitch.className === 'dot on') {
+    $lightSwitch.className = 'dot off';
+    $backgroundColor.className = 'container off';
+  } else if ($lightSwitch.className === 'dot off') {
+    $lightSwitch.className = 'dot on';
+    $backgroundColor.className = 'container on';
+  }
+});
+
+// $lightSwitch?.addEventListener('click', lightsOn);
+// function lightsOn(event: Event): void {
+//   if (!$lightSwitch) throw new Error('$lightSwitch does not exist!');
+//   if (!$backgroundColor) throw new Error('$backgroundColor does not exist!');
+//   $lightSwitch.className = 'dot off';
+//   $backgroundColor.className = 'container off';
+// }
