30a4f80fc3f426c2cfbc4dba8fe8e7743ad08bf8
Karen Bu <ktianbu@gmail.com>
2025-03-29 20:52:15 +0000

    Switching to main to reference other exercises

--- typescript-typing-tutor/main.js
+++ typescript-typing-tutor/main.js
@@ -1,1 +1,9 @@
 'use strict';
+// queryAll to get nodeList (acts like an array , can ask for i of the nodelist)
+// declare a variable to keep track of current position typed and if it matches the span textcontent
+const $letter = document.querySelector('.letter');
+console.log($letter);
+// if (!$letter) throw new Error('$letter does not exist!');
+// $letter?.addEventListener('keydown', (event: Event) => {
+//   console.log('key pressed');
+// });
---
+++ typescript-typing-tutor/main.ts
@@ -0,0 +1,11 @@
+// queryAll to get nodeList (acts like an array , can ask for i of the nodelist)
+// declare a variable to keep track of current position typed and if it matches the span textcontent
+
+const $letter = document.querySelector('.letter');
+console.log($letter);
+
+// if (!$letter) throw new Error('$letter does not exist!');
+
+// $letter?.addEventListener('keydown', (event: Event) => {
+//   console.log('key pressed');
+// });
