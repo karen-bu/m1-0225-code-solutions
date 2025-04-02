470e3c0f6b01dae6962651381b577fa5ef452337
Karen Bu <ktianbu@gmail.com>
2025-04-02 01:32:04 +0000

    Finished exercise typescript-typing-tutor

--- typescript-typing-tutor/main.ts
+++ typescript-typing-tutor/main.ts
@@ -1,24 +1,43 @@
 const $letter = document.querySelectorAll('.letter');
 if (!$letter) throw new Error('$letter does not exist!');
-// console.log($letter);
 
-// for (let i = 0; i < $letter.length; i++) {
-//   console.log($letter[i].textContent);
-// }
+const $modal = document.querySelector('dialog');
+if (!$modal) throw new Error('$modal does not exist!');
+
+const $accuracy = document.querySelector('.accuracy');
+if (!$accuracy) throw new Error('$accuracy does not exist!');
+
+let currentLetter = 0;
+let wrongLetters = 0;
+let totalLetters = 0;
+let gameOver = false;
 
 document.addEventListener('keydown', (event: KeyboardEvent) => {
   const keyPressed = event.key;
   console.log(event.key);
-
-  let currentLetter = 0;
-
+  if (gameOver === true) {
+    return;
+  }
   if (keyPressed === $letter[currentLetter].textContent) {
-    console.log('same hat!');
+    console.log('right letter!');
     $letter[currentLetter].className = 'letter correct';
+    if (currentLetter >= 35) {
+      gameOver = true;
+      const accuracy = (
+        ((totalLetters - wrongLetters) / totalLetters) *
+        100
+      ).toFixed(2);
+      $accuracy.textContent = accuracy.toString() + '%';
+      $modal.showModal();
+      return;
+    }
     $letter[currentLetter + 1].className = 'letter target';
     currentLetter++;
   } else {
     console.log('wrong letter!');
-    $letter[currentLetter].className = 'letter incorrect';
+    $letter[currentLetter].className = 'letter incorrect target';
+    wrongLetters++;
   }
+
+  totalLetters++;
 });
