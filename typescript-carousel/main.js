209b1ac1db6f3b4f7a6d701a2bc83da11e6de97f
Karen Bu <ktianbu@gmail.com>
2025-05-02 19:32:02 +0000

    added event listener to change imgSRC using left and right arrows

--- typescript-carousel/main.js
+++ typescript-carousel/main.js
@@ -1,11 +1,29 @@
-"use strict";
-const imgSRC = [
-    './images/001.png',
-    './images/004.png',
-    './images/07.png',
-    './images/025.png',
-    './images/039.png',
-];
+'use strict';
+let counter = 0;
 const $img = document.querySelector('img');
-$img.src =
-;
+const imgSRC = [
+  './images/001.png',
+  './images/004.png',
+  './images/007.png',
+  './images/025.png',
+  './images/039.png',
+];
+function srcSwap() {
+  if (!$img) throw new Error('$img does not exist!');
+  if (counter < imgSRC.length) {
+    $img.src = imgSRC[counter];
+    counter += 1;
+  } else counter = 0;
+}
+function srcSwapReverse() {
+  if (!$img) throw new Error('$img does not exist!');
+  if (counter > 0) {
+    $img.src = imgSRC[counter];
+    counter -= 1;
+  } else counter = imgSRC.length - 1;
+}
+// moving through carousel with left/right arrows
+const $leftArrow = document.querySelector('.fa-chevron-left');
+const $rightArrow = document.querySelector('.fa-chevron-right');
+$leftArrow?.addEventListener('click', srcSwapReverse);
+$rightArrow?.addEventListener('click', srcSwap);
--- typescript-carousel/main.ts
+++ typescript-carousel/main.ts
@@ -1,9 +1,33 @@
+let counter: number = 0;
+const $img = document.querySelector('img');
+
 const imgSRC: string[] = [
   './images/001.png',
   './images/004.png',
-  './images/07.png',
+  './images/007.png',
   './images/025.png',
   './images/039.png',
 ];
 
-const $img = document.querySelector('img') as HTMLImageElement;
+function srcSwap(): void {
+  if (!$img) throw new Error('$img does not exist!');
+  if (counter < imgSRC.length) {
+    $img.src = imgSRC[counter];
+    counter += 1;
+  } else counter = 0;
+}
+
+function srcSwapReverse(): void {
+  if (!$img) throw new Error('$img does not exist!');
+  if (counter > 0) {
+    $img.src = imgSRC[counter];
+    counter -= 1;
+  } else counter = imgSRC.length - 1;
+}
+
+// moving through carousel with left/right arrows
+const $leftArrow = document.querySelector('.fa-chevron-left');
+const $rightArrow = document.querySelector('.fa-chevron-right');
+
+$leftArrow?.addEventListener('click', srcSwapReverse);
+$rightArrow?.addEventListener('click', srcSwap);
