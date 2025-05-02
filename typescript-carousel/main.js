69867704460e714d4395b5336a8658475c3d6a85
Karen Bu <ktianbu@gmail.com>
2025-05-02 01:52:42 +0000

    stashing files to switch branches

--- typescript-carousel/main.js
+++ typescript-carousel/main.js
@@ -1,1 +1,11 @@
-'use strict';
+"use strict";
+const imgSRC = [
+    './images/001.png',
+    './images/004.png',
+    './images/07.png',
+    './images/025.png',
+    './images/039.png',
+];
+const $img = document.querySelector('img');
+$img.src =
+;
---
+++ typescript-carousel/main.ts
@@ -0,0 +1,9 @@
+const imgSRC: string[] = [
+  './images/001.png',
+  './images/004.png',
+  './images/07.png',
+  './images/025.png',
+  './images/039.png',
+];
+
+const $img = document.querySelector('img') as HTMLImageElement;
