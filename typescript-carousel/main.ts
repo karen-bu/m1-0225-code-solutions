7c862a9acd7efa054366513fc9a4741517244014
Karen Bu <ktianbu@gmail.com>
2025-05-03 00:46:11 +0000

    finished left and right arrow  manual scroll functions

--- typescript-carousel/main.ts
+++ typescript-carousel/main.ts
@@ -35,29 +35,27 @@ function srcSwap(): void {
 
 function srcSwapReverse(): void {
   if (!$img) throw new Error('$img does not exist!');
+  if (!$carouselCounter) throw new Error('$carouselCounter does not exist!');
 
-  if (index > 0) {
-    $img.src = imgSRC[index];
+  $img.src = imgSRC[index];
+  if (index >= 1) {
     index -= 1;
-    console.log('index variable:', index);
-    console.log('index of element:', imgSRC.indexOf(imgSRC[index]));
-  } else if (index === 0) {
-    index = 5;
-    $img.src = imgSRC[index - 1];
-    console.log(index);
+    $img.src = imgSRC[index];
+    $carouselCounter[index + 1].classList.remove('fa-solid');
+    $carouselCounter[index + 1].classList.add('fa-regular');
+    $carouselCounter[index].classList.remove('fa-regular');
+    $carouselCounter[index].classList.add('fa-solid');
+  } else {
+    index = 4;
+    $img.src = imgSRC[index];
+    $carouselCounter.forEach((counter) => counter.classList.remove('fa-solid'));
+    $carouselCounter.forEach((counter) => counter.classList.add('fa-regular'));
+    $carouselCounter[index].classList.add('fa-solid');
   }
 }
 
-// function srcSwapReverse(): void {
-//   if (!$img) throw new Error('$img does not exist!');
-//   if (counter > 0) {
-//     $img.src = imgSRC[counter];
-//     counter -= 1;
-//   } else counter = imgSRC.length - 1;
-// }
-
 // autotimed image slideshow
-// setInterval(srcSwap, 3000);
+setInterval(srcSwap, 3000);
 
 // moving through carousel with left/right arrows
 const $leftArrow = document.querySelector('.fa-chevron-left');
